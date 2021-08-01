acd48ab9e91074cb9360c4dd190c5b46c6c6c3d5
Redouane S <redouane.sobaihi@live.com>
2021-08-01 15:39:09 +0700

    feat: add update token name test

---
+++ test/contract.test.ts
@@ -0,0 +1,48 @@
+import { config } from 'dotenv'
+config()
+
+import Web3 from 'web3'
+import { Wallet } from 'ethers'
+
+import { abi } from '../assets/Bridge.json'
+
+const provider_uri = String(process.env.PROVIDER_URI)
+const contract_address = String(process.env.CONTRACT_ADDRESS)
+const mnemonic = String(process.env.MNEMONIC)
+
+
+const web3 = new Web3()
+const provider = new Web3.providers.WebsocketProvider(provider_uri)
+web3.setProvider(provider)
+
+const wallet = Wallet.fromMnemonic(mnemonic)
+web3.eth.accounts.privateKeyToAccount(wallet.privateKey)
+
+var contract = new web3.eth.Contract(<any>abi, contract_address, {
+    from: wallet.address
+});
+
+test('Update Token Name', async () => {
+
+    const method = contract.methods.updateTokenName('REDCOIN', 'Coin By Redouane S.')
+
+    const gas = await web3.eth.estimateGas({ 
+        from: wallet.address,
+        to: contract_address,
+        value: 0,
+        data: method.encodeABI()
+    });
+
+    const signedTx = await web3.eth.accounts.signTransaction({
+        from: wallet.address,
+        to: contract_address,
+        data: method.encodeABI(),
+        gas
+    }, wallet.privateKey)
+
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction!)
+
+    expect(receipt.status)
+    expect(receipt.transactionHash)
+
+}, 60000)
